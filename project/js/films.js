95a4f7f0781bad5da604ddd0f59bd5bcc7755a3f
Kitra <kitrarazin2020@u.northwestern.edu>
2020-06-10 17:37:57 -0500

    Checking in my project folder

--- project/js/films.js
+++ project/js/films.js
@@ -1,11 +1,11 @@
 //alert messages on load to force interaction and allow audio hover effect
-function hoverMessage() {
-  alert("Hover over the film posters to hear short score excerpts. Click on the film posters for more info about each film. (None of the 'watch film' buttons work yet.)");
-}
-
-function clickMessage() {
-  alert("Click on the film posters for more info about each film.");
-}
+//function hoverMessage() {
+//  alert("Hover over the film posters to hear short score excerpts.");
+//}
+//
+//function clickMessage() {
+//  alert("Click on the film posters for more info about each film.");
+//}
 
 //play audio on hover
 
@@ -20,7 +20,12 @@ function pauseAudio(id) {
 }
 
 //flip cards using jQuery
- $('.card .flipper').click(function() {
+ $('.card .flipper .front').click(function() {
+	$(this).closest('.card').toggleClass('hover');
+    $(this).css('transform, rotateY(180deg)');
+});
+
+ $('.card .flipper .back-button').click(function() {
 	$(this).closest('.card').toggleClass('hover');
     $(this).css('transform, rotateY(180deg)');
 });
@@ -30,3 +35,35 @@ function pauseAudio(id) {
 //  'placeholder.html',
 //  '_blank' // <- This is what makes it open in a new window.
 //);
+
+//div messages to force interaction and allow audio hover effect
+function divMessage() {
+//    var ok = document.querySelector('.message-b');
+//    ok.style.display = "none";
+//    var cards = document.querySelector('.cards');
+    cards.style.marginTop = "334px";
+    var front = document.getElementsByClassName('front');
+    var front = document.querySelector(".front");
+    front.style.opacity = "";
+    front.style.opacity = "1";
+    
+}
+
+ 
+
+$(document).ready(function(){
+  $(".ok").click(function(){
+    $(".message-b").fadeOut(500);
+      $("body").css("overflow", "auto");
+  });
+});
+
+//jQuery(document).ready(function($) {
+//    $(".ok").click(function(){
+//        $('.cards').animate({ marginTop: '334px' }, 2000);
+//    });
+//});
+
+
+
+//after we scroll so that cards marginTop is 100px, change marginTop to 100px permanently
--- project/js/test.js
+++ project/js/test.js
@@ -3,3 +3,19 @@
     $(this).css('transform, rotateY(180deg)');
 });
 
+$(document).ready(function(){
+  $(".btn1").click(function(){
+    $(".p").fadeOut(1000);
+  });
+  $(".btn2").click(function(){
+    $(".p").fadeIn(1000);
+  });
+});
+
+jQuery(document).ready(function($) {
+    $(".item").mouseleave(function(){
+        $(this).find('.info').stop().css('marginTop', '0');
+    }).mouseenter(function(){
+        $(this).find('.info').animate({ marginTop: '-50px', opacity: 0.5 }, 1000);
+    });
+});

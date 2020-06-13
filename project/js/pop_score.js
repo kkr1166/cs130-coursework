52439b2af80ea7c5504e651ff07f3a909f2012ef
Kitra <kitrarazin2020@u.northwestern.edu>
2020-06-12 19:06:14 -0500

    Checking in my project folder

--- project/js/pop_score.js
+++ project/js/pop_score.js
@@ -5,29 +5,29 @@ function viewScore(id) {
     if (x.style.display === "none") {
         x.style.display = "block";
         y.innerHTML = "";
-        y.innerHTML = "Hide Score"; 
+        y.innerHTML = "Hide Lyrics"; 
     }
     else {
         x.style.display = "none";
         y.innerHTML = "";
-        y.innerHTML = "View Score";        
+        y.innerHTML = "View Lyrics";        
     }   
 }
  
 
  
-const audioPlayer1 = AudioPlayer('.player.heterochromia', "../music/popMusic/dontThink.mp3");
+const audioPlayer1 = AudioPlayer('.player.dontthink', "../music/popMusic/dontThink.mp3");
 
-const audioPlayer2 = AudioPlayer('.player.bonesick', "../music/popMusic/affluentAphasia.mp3");
+const audioPlayer2 = AudioPlayer('.player.affluentaphasia', "../music/popMusic/affluentAphasia.mp3");
 
-const audioPlayer3 = AudioPlayer('.player.blacksugar', "../music/popMusic/faeries.mp3");
+const audioPlayer3 = AudioPlayer('.player.faeries', "../music/popMusic/faeries.mp3");
 
-const audioPlayer4 = AudioPlayer('.player.smokewater', "../music/popMusic/skeletonLies.mp3");
+const audioPlayer4 = AudioPlayer('.player.skeletonlies', "../music/popMusic/skeletonLies.mp3");
 
-const audioPlayer5 = AudioPlayer('.player.beehivecoke', "../music/popMusic/seaSharpe.mp3");
+const audioPlayer5 = AudioPlayer('.player.seasharpe', "../music/popMusic/seaSharpe.mp3");
 
-const audioPlayer6 = AudioPlayer('.player.mode', "../music/popMusic/roslynByBon.mp3");
+const audioPlayer6 = AudioPlayer('.player.roslyn', "../music/popMusic/roslynByBon.mp3");
 
-const audioPlayer7 = AudioPlayer('.player.tiptoe', "../music/popMusic/whyWait.mp3");
+const audioPlayer7 = AudioPlayer('.player.why', "../music/popMusic/whyWait.mp3");
 
-const audioPlayer8 = AudioPlayer('.player.rain', "../music/popMusic/riverByIbeyi.mp3");
+const audioPlayer8 = AudioPlayer('.player.river', "../music/popMusic/riverByIbeyi.mp3");

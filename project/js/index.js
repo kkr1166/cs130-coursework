2571d1c846f90b7f629fa554f4f28fc2feb417b0
Kitra <kitrarazin2020@u.northwestern.edu>
2020-06-13 16:05:21 -0500

    Checking in my project folder

--- project/js/index.js
+++ project/js/index.js
@@ -1,8 +1,18 @@
-//function myFunction() {
-//  document.getElementById("name").innerHTML = "";
-//}
-
-
 $(window).scroll(function(){
     $(".namediv").css("opacity", 1 - $(window).scrollTop() / 100);
   });
+
+
+
+
+
+
+//function bioScroll() {
+//    var bio = document.querySelector.getElementById("bio");
+//    var bioHeight = bio.offsetHeight;
+//    var navHeight = nav.offsetHeight;
+//    var second = document.querySelector.getElementsByClassName("second");
+//    second.style.height = "calc(100vh - navHeight - bioHeight)";
+//    console.log(bioHeight);   
+//}
+

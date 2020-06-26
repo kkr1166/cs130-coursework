df120b8924cce14a58280cc72c90a6c1a48f8a33
Karen Bao <karenbao2021@u.northwestern.edu>
2020-06-25 20:20:34 -0700

    other players pause when pressing play

--- project/js/pop.js
+++ project/js/pop.js
@@ -47,6 +47,13 @@ const AudioPlayer = (selector, audioFile) => {
     };
  
     const play = () => {
+        for (let item of document.getElementsByClassName('track')) {
+            item.pause();
+        }
+        for (let button of document.getElementsByClassName('play_pause')) {
+            button.classList.remove('fa-play', 'fa-pause');
+            button.classList.add('fa-play');
+        }
         playButton.classList.remove('fa-play', 'fa-pause');
         jumpToTime();
         audio.play();

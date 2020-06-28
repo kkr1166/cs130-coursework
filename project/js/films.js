6cd53b2d30c633b724aec0579920e694972a341b
Kitra <kitrarazin2020@u.northwestern.edu>
2020-06-28 12:39:00 -0500

    Checking in my project folder

--- project/js/films.js
+++ project/js/films.js
@@ -1,12 +1,28 @@
-//play corresponding audio when first poster is flipped- in HTML?
-//flip card button turns off music of corresponding poster
-//when click on new poster, all audio is paused before corresponding poster plays
-//when you click watch, all audio stops
 //maybe make cards flip to poster side when a new poster is clicked so you're not confused where the audio is coming from
 
 
 //play audio on hover
 function playAudio(id) {
+  var one = document.getElementById("audioone");
+  one.pause(); 
+  var two = document.getElementById("audiotwo");
+  two.pause();
+  var three = document.getElementById("audiothree");
+  three.pause();
+  var four = document.getElementById("audiofour");
+  four.pause();
+  var five = document.getElementById("audiofive");
+  five.pause();
+  var six = document.getElementById("audiosix");
+  six.pause();
+  var seven = document.getElementById("audioseven");
+  seven.pause();
+  var eight = document.getElementById("audioeight");
+  eight.pause();
+  var nine = document.getElementById("audionine");
+  nine.pause();
+  var ten = document.getElementById("audioten");
+  ten.pause();
   var audio = document.getElementById("audio" + id);
   audio.play();
 }
@@ -15,26 +31,34 @@ function pauseAudio(id) {
   var audio = document.getElementById("audio" + id);
   audio.pause();
 }
-//pause all audio
-function pauseAll() {
-    for (let item of document.getElementsByClassName('track')) {
-            item.pause();
-        }
-}
-
 
 //flip cards using jQuery
  $('.card .flipper .front').click(function() {
-	$(this).closest('.card').toggleClass('hover');
+     ///have all cards flip to the front
+     ///remove class .hover from all cards, then add class hover to current card
+    $('#one').removeClass('hover');
+    $('#two').removeClass('hover');
+    $('#three').removeClass('hover');
+    $('#four').removeClass('hover');
+    $('#five').removeClass('hover');
+    $('#six').removeClass('hover');
+    $('#seven').removeClass('hover');
+    $('#eight').removeClass('hover');
+    $('#nine').removeClass('hover');
+    $('#ten').removeClass('hover');
+	$(this).closest('.card').addClass('hover');
     $(this).css('transform, rotateY(180deg)');
-     //pause any playing audio
-     //play corrrseponding audio
+    
+     ////
+     ////
+     ///
+     ////
+     ////
 });
 
- $('.card .flipper .back-button').click(function() {
-	$(this).closest('.card').toggleClass('hover');
+ $('.card .flipper .back').click(function() {
+	$(this).closest('.card').removeClass('hover');
     $(this).css('transform, rotateY(180deg)');
-     //pause corrseponding audio
 });
  
 //fade out ok

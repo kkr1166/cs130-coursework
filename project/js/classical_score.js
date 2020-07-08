7e46b88d9fae04b2a514f545586c6c5798e77c91
Kitra <kitrarazin2020@u.northwestern.edu>
2020-07-08 17:24:35 -0500

    rerouting mp3s and m4as

--- project/js/classical_score.js
+++ project/js/classical_score.js
@@ -104,30 +104,30 @@ function viewParts(id) {
 }
 
 
-const audioPlayer1 = AudioPlayer('.player.heterochromia', "../music/classicalMusic/heterochromia.mp3");
+const audioPlayer1 = AudioPlayer('.player.heterochromia', "../../music/classicalMusic/heterochromia.mp3");
 
 //const audioPlayer1 = AudioPlayer('.player.heterochromia', "../music/classicalMusic/heterochromia.mp3");
 
-const audioPlayer2 = AudioPlayer('.player.bonesick', "../../music/classicalMusic/rain.mp3");
-
-const audioPlayer3 = AudioPlayer('.player.blacksugar', "../music/classicalMusic/rain.mp3");
-
-const audioPlayer4 = AudioPlayer('.player.smokewater', "../music/classicalMusic/rain.mp3");
-
-const audioPlayer5 = AudioPlayer('.player.beehivecoke', "../music/classicalMusic/rain.mp3");
-
-//const audioPlayer2 = AudioPlayer('.player.bonesick', "../music/classicalMusic/boneSick.m4a");
+//const audioPlayer2 = AudioPlayer('.player.bonesick', "../../music/classicalMusic/rain.mp3");
 //
-//const audioPlayer3 = AudioPlayer('.player.blacksugar', "../music/classicalMusic/blackSugar.m4a");
+//const audioPlayer3 = AudioPlayer('.player.blacksugar', "../music/classicalMusic/rain.mp3");
 //
-//const audioPlayer4 = AudioPlayer('.player.smokewater', "../music/classicalMusic/smokeWater.m4a");
+//const audioPlayer4 = AudioPlayer('.player.smokewater', "../music/classicalMusic/rain.mp3");
 //
-//const audioPlayer5 = AudioPlayer('.player.beehivecoke', "../music/classicalMusic/beehiveCoke.m4a");
+//const audioPlayer5 = AudioPlayer('.player.beehivecoke', "../music/classicalMusic/rain.mp3");
 
-const audioPlayer6 = AudioPlayer('.player.mode', "../music/classicalMusic/mode.mp3");
+const audioPlayer2 = AudioPlayer('.player.bonesick', "../../music/classicalMusic/boneSick.m4a");
 
-const audioPlayer7 = AudioPlayer('.player.tiptoe', "../music/classicalMusic/tiptoe.mp3");
+const audioPlayer3 = AudioPlayer('.player.blacksugar', "../../music/classicalMusic/blackSugar.m4a");
 
-const audioPlayer8 = AudioPlayer('.player.rain', "../music/classicalMusic/rain.mp3");
+const audioPlayer4 = AudioPlayer('.player.smokewater', "../../music/classicalMusic/smokeWater.m4a");
 
-const audioPlayer9 = AudioPlayer('.player.mourningmoon', "../music/classicalMusic/mourningMoon.mp3");
+const audioPlayer5 = AudioPlayer('.player.beehivecoke', "../../music/classicalMusic/beehiveCoke.m4a");
+
+const audioPlayer6 = AudioPlayer('.player.mode', "../../music/classicalMusic/mode.mp3");
+
+const audioPlayer7 = AudioPlayer('.player.tiptoe', "../../music/classicalMusic/tiptoe.mp3");
+
+const audioPlayer8 = AudioPlayer('.player.rain', "../../music/classicalMusic/rain.mp3");
+
+const audioPlayer9 = AudioPlayer('.player.mourningmoon', "../../music/classicalMusic/mourningMoon.mp3");
--- project/js/pop_score.js
+++ project/js/pop_score.js
@@ -241,18 +241,18 @@ function logicriver(id) {
     }   
 }
 
-const audioPlayer1 = AudioPlayer('.player.dontthink', "../music/popMusic/dontThink.mp3");
+const audioPlayer1 = AudioPlayer('.player.dontthink', "../../music/popMusic/dontThink.mp3");
 
-const audioPlayer2 = AudioPlayer('.player.affluentaphasia', "../music/popMusic/affluentAphasia.mp3");
+const audioPlayer2 = AudioPlayer('.player.affluentaphasia', "../../music/popMusic/affluentAphasia.mp3");
 
-const audioPlayer3 = AudioPlayer('.player.faeries', "../music/popMusic/faeries.mp3");
+const audioPlayer3 = AudioPlayer('.player.faeries', "../../music/popMusic/faeries.mp3");
 
-const audioPlayer4 = AudioPlayer('.player.skeletonlies', "../music/popMusic/skeletonLies.mp3");
+const audioPlayer4 = AudioPlayer('.player.skeletonlies', "../../music/popMusic/skeletonLies.mp3");
 
-const audioPlayer5 = AudioPlayer('.player.seasharpe', "../music/popMusic/seaSharpe.mp3");
+const audioPlayer5 = AudioPlayer('.player.seasharpe', "../../music/popMusic/seaSharpe.mp3");
 
-const audioPlayer6 = AudioPlayer('.player.roslyn', "../music/popMusic/roslynByBon.mp3");
+const audioPlayer6 = AudioPlayer('.player.roslyn', "../../music/popMusic/roslynByBon.mp3");
 
-const audioPlayer7 = AudioPlayer('.player.why', "../music/popMusic/whyWait.mp3");
+const audioPlayer7 = AudioPlayer('.player.why', "../../music/popMusic/whyWait.mp3");
 
-const audioPlayer8 = AudioPlayer('.player.river', "../music/popMusic/riverByIbeyi.mp3");
+const audioPlayer8 = AudioPlayer('.player.river', "../../music/popMusic/riverByIbeyi.mp3");
